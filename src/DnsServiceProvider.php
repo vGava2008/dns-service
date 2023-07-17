a18c01d6c93151fcb3b337c4afa9dc96da8e1225
viacheslav.gava <viacheslav.gava.devmrks@gmail.com>
2023-07-17 22:46:58 +0300

    include service provider

--- src/DnsServiceProvider.php
+++ src/DnsServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace Vyacheslavhava\DnsService;
 
-class DnsServiceProvider
-{
+use Illuminate\Support\ServiceProvider;
 
+class DnsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(DnsServiceInterface::class, function($app){
+            return new DnsService();
+        });
+    }
 }
